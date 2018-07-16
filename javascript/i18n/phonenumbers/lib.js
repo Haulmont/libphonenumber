2907b8a7a49da464f188be26731b443fb7a1a47a
Stepanov Max <stepanov@haulmont.com>
2018-07-16 13:28:19 +0400

    Ability to create external js lib

---
+++ javascript/i18n/phonenumbers/lib.js
@@ -0,0 +1,5 @@
+goog.require('i18n.phonenumbers.PhoneNumberFormat');
+goog.require('i18n.phonenumbers.PhoneNumberType');
+goog.require('i18n.phonenumbers.PhoneNumberUtil');
+goog.require('i18n.phonenumbers.PhoneNumberUtil.ValidationResult');
+goog.exportSymbol('PNF', i18n.phonenumbers);
